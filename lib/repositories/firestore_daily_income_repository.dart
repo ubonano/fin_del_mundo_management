f570767eeb896eda8471d8a3d21c33643a9763ec
Uriel Bonano <ubonano@gmail.com>
2023-05-09 22:02:46 -0300

    loggin reposotory

--- lib/repositories/firestore_daily_income_repository.dart
+++ lib/repositories/firestore_daily_income_repository.dart
@@ -1,28 +1,58 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/daily_income.dart';
 import '../utils/interfaces/daily_income_repository.dart';
+import 'package:logging/logging.dart';
 
 class FirestoreDailyIncomeRepository implements DailyIncomeRepository {
+  final _logger = Logger('FirestoreDailyIncomeRepository');
   final _collection = FirebaseFirestore.instance.collection('dailyIncomes');
 
   @override
   Stream<List<DailyIncome>> getAll() {
-    return _collection.snapshots().map((query) =>
-        query.docs.map((doc) => DailyIncome.fromDocument(doc)).toList());
+    _logger.info('Starting to fetch all daily incomes');
+    return _collection.snapshots().map((query) {
+      final incomes =
+          query.docs.map((doc) => DailyIncome.fromDocument(doc)).toList();
+      _logger.info('Finished fetching all daily incomes');
+      return incomes;
+    });
   }
 
   @override
   Future<void> add(DailyIncome income) {
-    return _collection.add(income.toDocument());
+    _logger.info('Starting to add a daily income');
+    return _collection
+        .add(income.toDocument())
+        .then((value) => _logger.info('Successfully added a daily income'))
+        .catchError((error) {
+      _logger.severe('Failed to add a daily income: $error');
+      throw error;
+    });
   }
 
   @override
   Future<void> update(DailyIncome income) {
-    return _collection.doc(income.id).update(income.toDocument());
+    _logger.info('Starting to update a daily income');
+    return _collection
+        .doc(income.id)
+        .update(income.toDocument())
+        .then((value) => _logger.info('Successfully updated a daily income'))
+        .catchError((error) {
+      _logger.severe('Failed to update a daily income: $error');
+      throw error;
+    });
   }
 
   @override
   Future<void> delete(DailyIncome income) {
-    return _collection.doc(income.id).delete();
+    _logger.info('Starting to delete a daily income');
+    return _collection
+        .doc(income.id)
+        .delete()
+        .then((value) => _logger.info('Successfully deleted a daily income'))
+        .catchError((error) {
+      _logger.severe('Failed to delete a daily income: $error');
+      throw error;
+    });
   }
 }
