a457b53e6bd898b22d4eb0fa2383c1d0147a44c8
Uriel Bonano <ubonano@gmail.com>
2023-05-09 21:59:52 -0300

    router setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,70 +2,63 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'setup/firebase_options.dart';
+import 'setup/router.dart';
 
 Future<void> main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp.router(
+      title: 'Fin del Mundo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.purple),
+      routerConfig: _appRouter.config(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+/**
+ Responde como un experto en Flutter, firebase, firestore, getIt, logging, rxdart, clean code, principios SOLID, auto_route. Tu tarea es escribir codigo de promgramacion segun el requisito que te hago. 
+ */
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+/**
+ implementa la funcionalidad para crear nuevos ingresos diarios. Esto incluye la validación de los datos ingresados por el usuario y la actualización de la pantalla para mostrar los nuevos ingresos diarios.
+Se debe poder crear nuevas mesas. La informacion necesaria es:
+{
+  "createdAt": timestamp,      // Fecha en la que se almacena el ingreso diario, este dato se debe cargar automaticamente. No se modifica. No se muestra al usuario.
+  "modifiedAt": timestamp,     // Fecha en la que se modificó el ingreso, este dato se modifica unicamente de forma automatica al actualizar el ingreso diario. No se muestra al usuario.
+  "createdBy": string,         // Usuario que creó el ingreso, este dato se debe cargar automaticamente por el usuario que crea el ingreso diario. No se debe poder modificar y no se muestra al usuario.
+  "modifiedBy": string,        // Usuario que modificó el ingreso, este dato se modifica unicamente y de forma automatica al actualizar un ingreso diario.
+  "date": timestamp,           // Fecha que corresponde al ingreso
+  "branch": string,            // Sucursal del ingreso
+  "total": number,             // Total de ingresos del día. Este dato se calcula automaticamente sumando los metodos de pago, el sobrante y restando el faltante. No se puede modificar manualmente.
+  "paymentMethods": {          // Métodos de pago
+    "cash": number,            // Dinero que ingresó en efectivo
+    "cards": number,           // Dinero que ingresó mediante tarjetas
+    "mercadoPago": number      // Dinero que ingresó mediante MercadoPago
+  },
+  "surplus": number,           // Sobrante
+  "shortage": number           // Faltante
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
+Se debe poder eliminar ingresos diarios, se debe confirmar si el usuario desea eliminar.
+Se debe poder modificar la informacion de los ingresos diarios.
+Para la base de dato usa Firestore.
+Para manejar las instancias utiliza la dependencia GetIt, la configuracion debe estar en un archivo aparte.
+Para integrar el controller y la interfaz grafica usa BehaviorSubject.
+Utiliza auto_route las rutas.
+Agrega logs en la pantalla, en el controller y el repositorio, antes y despues de cada accion que permitan una buena depuracion utilizando loggin.
+El codigo de progrmaacion debe seguir los principios Clean Code y Solid. 
+El codigo debe reutilizable y debe ser facil de modificar en futuras actualizaciones.
+Crea un repositorio y un controller para las mesas.
+Para una mejor mantencion crea una interfaz del repositorio para facilitar el interacmbio de repositorios.
+Escribe todo el codigo, no des solamente la estructura. Implementa todo el codigo necesario para cumplir con todo lo indicado
+ */
