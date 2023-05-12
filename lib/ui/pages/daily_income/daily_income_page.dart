2fe02d2e0b558fab5cc31950a37f618d639070ca
Uriel Bonano <ubonano@gmail.com>
2023-05-11 23:06:21 -0300

    add login to daily income page

--- lib/ui/pages/daily_income/daily_income_page.dart
+++ lib/ui/pages/daily_income/daily_income_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:fin_del_mundo_management/setup/router.gr.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 import '../../../controllers/daily_income_controller.dart';
 import '../../../models/daily_income.dart';
 import '../../../setup/get_it_setup.dart';
@@ -17,34 +18,60 @@ class DailyIncomePage extends StatefulWidget {
 }
 
 class _DailyIncomePageState extends State<DailyIncomePage> {
+  final _logger = Logger('DailyIncomePage');
   final _controller = getIt<DailyIncomeController>();
   late final StackRouter? router;
 
   @override
   Widget build(BuildContext context) {
     router = AutoRouter.of(context);
+    _logger.info('Building DailyIncomePage');
 
     return Scaffold(
       appBar: AppBar(title: const Text('Ingresos diarios')),
       body: AppStreamBuilder<List<DailyIncome>>(
         stream: _controller.incomes,
-        onData: (data) => DailyIncomeList(
-          incomes: data,
-          onDeletePressed: (income) {
-            AppDialog.showDelete(
-              context,
-              onPressed: () {
-                _controller.delete(income);
-                router?.pop();
-              },
-            );
-          },
-        ),
+        onData: (data) {
+          _logger.info('Received daily incomes data');
+          return DailyIncomeList(
+            incomes: data,
+            onDeletePressed: (income) {
+              _logger
+                  .info('Delete button pressed for daily income: ${income.id}');
+              AppDialog.showDelete(
+                context,
+                onPressed: () => onDeletePressed(income),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () => router?.push(const DailyIncomeFormRoute()),
+        onPressed: () {
+          _logger.info('Add button pressed');
+          router?.push(const DailyIncomeFormRoute());
+        },
       ),
+    );
+  }
+
+  void onDeletePressed(DailyIncome income) {
+    _logger.info('Deleting daily income: ${income.id}');
+    try {
+      _controller.delete(income);
+      router?.pop();
+      _logger.info('Daily income deleted successfully');
+    } catch (e) {
+      _logger.severe('Error deleting daily income: ${e.toString()}');
+      _showSnackbar('An error occurred');
+    }
+  }
+
+  void _showSnackbar(String message) {
+    _logger.info('Showing snackbar with message: $message');
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
     );
   }
 }
