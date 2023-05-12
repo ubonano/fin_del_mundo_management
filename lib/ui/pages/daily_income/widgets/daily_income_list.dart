b1e8555db9020a7bdb515bedd8961c83e8d3ac38
Uriel Bonano <ubonano@gmail.com>
2023-05-12 00:39:30 -0300

    loggin daily income list

--- lib/ui/pages/daily_income/widgets/daily_income_list.dart
+++ lib/ui/pages/daily_income/widgets/daily_income_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart'; // Import logging
 import '../../../../models/daily_income.dart';
 import 'daily_income_tile.dart';
 
@@ -7,26 +8,38 @@ class DailyIncomeList extends StatefulWidget {
   final void Function(DailyIncome income)? onDeletePressed;
 
   const DailyIncomeList({
-    super.key,
+    Key? key,
     required this.incomes,
     this.onDeletePressed,
-  });
+  }) : super(key: key);
 
   @override
   State<DailyIncomeList> createState() => _DailyIncomeListState();
 }
 
 class _DailyIncomeListState extends State<DailyIncomeList> {
+  final _logger = Logger('DailyIncomeList');
+
   @override
   Widget build(BuildContext context) {
+    _logger
+        .info('Building DailyIncomeList with ${widget.incomes.length} incomes');
     return ListView.builder(
       itemCount: widget.incomes.length,
-      itemBuilder: (context, index) => DailyIncomeTile(
-        income: widget.incomes[index],
-        onDeletePressed: widget.onDeletePressed != null
-            ? () => widget.onDeletePressed!(widget.incomes[index])
-            : null,
-      ),
+      itemBuilder: (context, index) {
+        final income = widget.incomes[index];
+        _logger.info('Building DailyIncomeTile for income id: ${income.id}');
+        return DailyIncomeTile(
+          income: income,
+          onDeletePressed: widget.onDeletePressed != null
+              ? () {
+                  _logger.info(
+                      'Delete button pressed for income id: ${income.id}');
+                  widget.onDeletePressed!(income);
+                }
+              : null,
+        );
+      },
     );
   }
 }
