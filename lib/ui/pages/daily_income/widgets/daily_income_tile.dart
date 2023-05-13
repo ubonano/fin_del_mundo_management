6b654db2a9a4f9cca0961dc4d798201944664ad2
Uriel Bonano <ubonano@gmail.com>
2023-05-12 22:42:47 -0300

    refactor

--- lib/ui/pages/daily_income/widgets/daily_income_tile.dart
+++ lib/ui/pages/daily_income/widgets/daily_income_tile.dart
@@ -1,15 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:intl/intl.dart';
 import 'package:logging/logging.dart';
 import '../../../../models/daily_income.dart';
 import '../../../../setup/router.gr.dart';
+import '../../../../utils/app_formaters.dart';
 import 'daily_income_delete_icon_button.dart';
-
-extension CapExtension on String {
-  String get inCaps => '${this[0].toUpperCase()}${substring(1)}';
-}
 
 class DailyIncomeTile extends StatefulWidget {
   final DailyIncome income;
@@ -37,7 +33,8 @@ class _DailyIncomeTileState extends State<DailyIncomeTile> {
     _logger.info('Building DailyIncomeTile for income id: ${widget.income.id}');
 
     return ListTile(
-      title: Text('${_getDateFormatted()} - \$${_getFormattedTotal()}'),
+      title: Text(
+          '${AppFormaters.getFormattedDate(widget.income.date)} - \$${AppFormaters.getFormattedTotal(widget.income.total)}'),
       subtitle: Text(widget.income.branch),
       onTap: () {
         _logger.info('Tile pressed for daily income: ${widget.income.id}');
@@ -45,19 +42,5 @@ class _DailyIncomeTileState extends State<DailyIncomeTile> {
       },
       trailing: DailyIncomeDeleteIconButton(income: widget.income),
     );
-  }
-
-  String _getDateFormatted() {
-    var formattedDate =
-        DateFormat('EEEE dd-MM-yyyy', 'es_ES').format(widget.income.date);
-    var day = formattedDate.split(' ')[0].inCaps;
-    var date = formattedDate.split(' ').sublist(1).join(' ');
-
-    return '$day $date';
-  }
-
-  String _getFormattedTotal() {
-    final formatter = NumberFormat('#,###.00', 'es_AR');
-    return formatter.format(widget.income.total);
   }
 }
---
+++ lib/utils/app_formaters.dart
@@ -0,0 +1,20 @@
+import 'package:intl/intl.dart';
+
+extension CapExtension on String {
+  String get inCaps => '${this[0].toUpperCase()}${substring(1)}';
+}
+
+class AppFormaters {
+  static String getFormattedDate(DateTime dateTime) {
+    var formattedDate = DateFormat('EEEE dd-MM-yyyy', 'es_ES').format(dateTime);
+    var day = formattedDate.split(' ')[0].inCaps;
+    var date = formattedDate.split(' ').sublist(1).join(' ');
+
+    return '$day $date';
+  }
+
+  static String getFormattedTotal(dynamic total) {
+    final formatter = NumberFormat('#,###.00', 'es_AR');
+    return formatter.format(total);
+  }
+}
