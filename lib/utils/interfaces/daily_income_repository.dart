2689b673aa88c19ba7af9bcb8e5d4ab1b876d40b
Uriel Bonano <ubonano@gmail.com>
2023-05-15 00:02:54 -0300

    validation existing income

--- lib/utils/interfaces/daily_income_repository.dart
+++ lib/utils/interfaces/daily_income_repository.dart
@@ -3,6 +3,7 @@ import '../../models/daily_income.dart';
 abstract class DailyIncomeRepository {
   Stream<List<DailyIncome>> getAll();
   Stream<List<DailyIncome>> getByMonthAndYear(int month, int year);
+  Stream<List<DailyIncome>> getByDateAndBranch(DateTime date, String branch);
   Future<void> add(DailyIncome income);
   Future<void> update(DailyIncome income);
   Future<void> delete(DailyIncome income);
